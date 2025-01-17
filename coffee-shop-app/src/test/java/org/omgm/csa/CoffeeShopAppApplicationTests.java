fd381a2f68c505dfb70b1353f00de3cc4bc0ea8b
Libertrim <oscar.garciameza@icloud.com>
2025-01-16 21:13:38 -0600

    Crear proyecto con Spring Boot Initializer.

---
+++ coffee-shop-app/src/test/java/org/omgm/csa/CoffeeShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.omgm.csa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
